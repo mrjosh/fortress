5f1df70c462d8ecd161e6663b9dc227f554cb4f9
Alireza Josheghani <josheghani.dev@gmail.com>
2021-07-23 05:37:16 +0430

    Update ci/cd stages

--- cmds/version.go
+++ cmds/version.go
@@ -12,7 +12,7 @@ func newVersionCommand() *cobra.Command {
 		Use:   "version",
 		Short: "Print version information",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Fprintf(cmd.OutOrStdout(), "Fortress v0.0.1\n")
+			fmt.Fprintf(cmd.OutOrStdout(), "FORTRESS v0.0.1-dev\n")
 			fmt.Fprintf(cmd.OutOrStdout(), "Build type: Release\n")
 			fmt.Fprintf(cmd.OutOrStdout(), "Go version: %s\n", runtime.Version())
 			return nil
